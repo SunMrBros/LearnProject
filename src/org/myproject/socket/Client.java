ed91107d1e24b48eb8856763461ddc198140985d
kangzheng <KangZheng@KangZhengdeMacBook-Pro.local>
2017-01-05 23:35:37 +0800

    Socket编程开始
    定义服务器端ServiceSocket 和客户端Socket

---
+++ src/org/myproject/socket/Client.java
@@ -0,0 +1,7 @@
+package org.myproject.socket;
+
+public class Client {
+	
+	public static void main(String[] args){}
+
+}
---
+++ src/org/myproject/socket/ServiceClient.java
@@ -0,0 +1,10 @@
+package org.myproject.socket;
+
+public class ServiceClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
